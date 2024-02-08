41a892947826f35adea3f25848bb1647369bb6f3
jaehoon <go2play2@gmail.com>
2024-02-08 22:01:34 +0900

    Initial version

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+
+
+app = Flask(__name__)
+app.users = {}
+app.id_count = 1
+
+
+@app.route("/sign-up", methods=['POST'])
+def sign_up():
+    new_user = request.json
+    new_user["id"] = app.id_count
+    app.users[new_user["id"]] = new_user
+    app.id_count += 1
+
+    return jsonify(new_user)
+
+
+@app.route("/ping", methods=['GET'])
+def ping():
+    return "pong"
